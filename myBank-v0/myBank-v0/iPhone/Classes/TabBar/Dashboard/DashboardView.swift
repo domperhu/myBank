c0617510fc76d4387b8ce7dc54223659bafe3aee
dominique-perez_globant <dominique.perez@globant.com>
2024-02-18 23:09:20 -0500

    Add DownloadManager

--- myBank-v0/myBank-v0/iPhone/Classes/TabBar/Dashboard/DashboardView.swift
+++ myBank-v0/myBank-v0/iPhone/Classes/TabBar/Dashboard/DashboardView.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class DashboardView: UIView {
     
+    @IBOutlet private weak var btnProfile   : UIButton!
     @IBOutlet private weak var lblFullName  : UILabel!
     @IBOutlet private weak var lblEmail     : UILabel!
     
     func setInformationFrom(_ user: User) {
+        self.btnProfile.imageView?.downloadImageWithThis("https://bit.ly/4bIE4m3", awaitImage: UIImage(systemName: "person.circle"))
         self.lblFullName.text   = user.fullname
         self.lblEmail.text      = user.email.toSecureEmail()
     }
